0826a1d5107b58e5b289ed972bee00eec1e29722
Michael Bruce <michael.bruce@singletracksystems.com>
2015-09-29 20:34:43 +0100

    initial demo loop

--- src/adventure_game/core.clj
+++ src/adventure_game/core.clj
@@ -1,21 +1,45 @@
 (ns adventure-game.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def player-state
+  [{:room "room 1"
+    :health "healthy"}])
+
+(def rooms
+  [{:name "room 1"
+    :forward "room 2"
+    :backward "room 3"
+    :description "A dark room..."}
+   {:name "room 2"
+    :forward "room 3"
+    :backward "room 1"
+    :description "A darker room..."}
+   {:name "room 3"
+    :forward "room 1"
+    :backward "room 2"
+    :description "The darkest room..."}])
 
 (defn move
   "I move the player to a new room"
   [x]
   (println x "Hello, World!"))
 
-(defn game-loop [input]
-  (prn input)
+(defn action [input]
+  "performing game action"
+  (prn (cond (= input "forward") "moving forward"
+        (= input "backward") "moving backward"
+        :else :do-that-other-thing))
+  )
+
+;; (defn move [direction]
+;;   (cond (= direction "forward") "state"
+
+(defn game-loop [input state]
+  ;; (prn input)
   (if (not (= "exit" input))
-    (game-loop (read-line))
+    (let [new-state (action input)]
+    (game-loop (read-line) new-state))
     (prn "Exiting...")))
 
 (defn -main [& args]
-  (game-loop "start"))
+  (game-loop "start" player-state))
 
